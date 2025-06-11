28c879ac5cff2b1c5abc765473a0f06d4a79e5a7
LargeLow <joelowrymail@gmail.com>
2025-06-11 16:57:53 -0600

    Delete api/index.js

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Export the Express app for Vercel serverless deployment
-const path = require('path');
-
-// Set environment variable for Vercel detection
-process.env.VERCEL = '1';
-
-module.exports = require('../dist/index.js');
